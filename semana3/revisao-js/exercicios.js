cba305a35f8c74fa98fb624ffe89dc611a0e5774
Lucas Matheus Fernandes Gonçalves <lucas_matheus22@hotmail.com>
2021-06-28 15:46:04 -0300

    Exercício 12

--- semana3/revisao-js/exercicios.js
+++ semana3/revisao-js/exercicios.js
@@ -142,7 +142,15 @@ function ordenaArray(array) {
 }
 
 // EXERCÍCIO 12
-function filmeFavorito() {}
+function filmeFavorito() {
+  const filme = {
+    nome: "O Diabo Veste Prada",
+    ano: 2006,
+    diretor: "David Frankel",
+    atores: ["Meryl Streep", "Anne Hathaway", "Emily Blunt", "Stanley Tucci"],
+  };
+  return filme;
+}
 
 // EXERCÍCIO 13
 function imprimeChamada() {
