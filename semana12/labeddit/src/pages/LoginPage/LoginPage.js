5e51a2f12aefbd4a359cd553191222f7c61f025e
Lucas Fernandes <lucas_matheus22@hotmail.com>
2021-09-01 19:02:29 -0300

    Formulários e Validação

--- semana12/labeddit/src/pages/LoginPage/LoginPage.js
+++ semana12/labeddit/src/pages/LoginPage/LoginPage.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { ScreenContainer, SignUpButtonContainer } from "./styled";
+import { Button } from "@material-ui/core";
+import LoginForm from "./LoginForm";
+import { useHistory } from "react-router";
+import { goToSignUp } from "../../routes/coordinator";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const history = useHistory();
+  return (
+    <ScreenContainer>
+      <LoginForm />
+      <SignUpButtonContainer>
+        <Button
+          onClick={() => goToSignUp(history)}
+          type={"submit"}
+          fullWidth
+          variant={"outlined"}
+          color={"primary"}
+        >
+          Cadastrar-se
+        </Button>
+      </SignUpButtonContainer>
+    </ScreenContainer>
+  );
 };
 
 export default LoginPage;
