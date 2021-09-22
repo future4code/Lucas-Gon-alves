21969f858ace63e3648485e2efe1a04b035fb22d
Lucas Fernandes <lucas_matheus22@hotmail.com>
2021-09-22 15:40:21 -0300

    Exercício 3

--- semana15/express/src/index.ts
+++ semana15/express/src/index.ts
@@ -35,3 +35,26 @@ app.get("/countries/:id", (req: Request, res: Response) => {
     res.status(404).send("Not found");
   }
 });
+
+// Exercício 3
+app.get("/countries/search", (req: Request, res: Response) => {
+  let result: country[] = countries;
+
+  if (req.query.name) {
+    result = result.filter((country) =>
+      country.name.includes(req.query.name as string)
+    );
+  }
+
+  if (req.query.capital) {
+    result = result.filter((country) =>
+      country.capital.includes(req.query.capital as string)
+    );
+  }
+
+  if (req.query.continent) {
+    result = result.filter((country) =>
+      country.continent.includes(req.query.continent as string)
+    );
+  }
+});
