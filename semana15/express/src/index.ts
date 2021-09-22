20ab9a3cb732f05b4ec99d06ec4b80235fd69f15
Lucas Fernandes <lucas_matheus22@hotmail.com>
2021-09-22 14:55:17 -0300

    Exercício 2

--- semana15/express/src/index.ts
+++ semana15/express/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import { AddressInfo } from "net";
 import { countries } from "./data";
+import { country } from "./types";
 
 const app = express();
 
@@ -21,4 +22,16 @@ const server = app.listen(process.env.PORT || 3003, () => {
 
 app.get("/countries", (req: Request, res: Response) => {
   res.status(200).send(countries);
+});
+
+// Exercício 2
+app.get("/countries/:id", (req: Request, res: Response) => {
+  const result: country | undefined = countries.find(
+    (country) => country.id === Number(req.params.id)
+  );
+  if (result) {
+    res.status(200).send(result);
+  } else {
+    res.status(404).send("Not found");
+  }
 });
